92436c8bdecfe9284a18fc7a5e674d5e70c345c0
Ridoy <ridoy014@gmail.com>
2024-05-21 21:25:37 +0200

    User1 wrote second failing test for input [2, 2, 3, 3]

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -10,4 +10,11 @@ describe('Electrons Around the Cores', function() {
     it('should return 6 for [1, 2, 3, 4, 5]', function() {
         expect(calculator.electronsAroundCores([1, 2, 3, 4, 5])).toEqual(6);
     });
+
+
+    it('should return 4 for [2, 2, 3, 3]', function() {
+    expect(calculator.electronsAroundCores([2, 2, 3, 3])).toEqual(4);
+    });
+
+
 });
