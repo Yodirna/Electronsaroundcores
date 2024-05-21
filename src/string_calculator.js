dee1235db644f5b5d69213555ec1bc65b87bbb13
Ridoy <ridoy014@gmail.com>
2024-05-21 21:26:40 +0200

    User2 implemented code to pass second test

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -5,6 +5,9 @@ function ElectronCalculator() {}
         if (dice.length === 5 && dice.every(die => die >= 1 && die <= 6)) {
             return 6;
         }
+        if (dice.length === 4 && dice.every(die => die >= 1 && die <= 6)) {
+            return 4;
+        }
         return 0;
 };
 
