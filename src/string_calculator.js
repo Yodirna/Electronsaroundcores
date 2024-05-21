5f86391877263db2a87376fa9bfbb81781c7e492
Ridoy <ridoy014@gmail.com>
2024-05-21 21:34:28 +0200

    User2 Implemented simplest production code for all tests and refactored code

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,13 +1,8 @@
 
-function ElectronCalculator() {}
-    ElectronCalculator.prototype.electronsAroundCores = function(dice) {
-        // Implemented the simplest code to pass the test
-        if (dice.length === 5 && dice.every(die => die >= 1 && die <= 6)) {
-            return 6;
-        }
-        if (dice.length === 4 && dice.every(die => die >= 1 && die <= 6)) {
-            return 4;
-        }
-        return 0;
-};
 
+function ElectronCalculator() {}
+
+
+ElectronCalculator.prototype.electronsAroundCores = function(dice) {
+    return dice.reduce((a, b) => a + (b & 1 ? b - 1 : 0), 0);
+};
