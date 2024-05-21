32f103b3dba1a955df643cceb66353f27cde2718
Ridoy <ridoy014@gmail.com>
2024-05-21 21:29:54 +0200

    User1 implemented new failing tests

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -11,10 +11,15 @@ describe('Electrons Around the Cores', function() {
         expect(calculator.electronsAroundCores([1, 2, 3, 4, 5])).toEqual(6);
     });
 
-
     it('should return 4 for [2, 2, 3, 3]', function() {
-    expect(calculator.electronsAroundCores([2, 2, 3, 3])).toEqual(4);
+        expect(calculator.electronsAroundCores([2, 2, 3, 3])).toEqual(4);
     });
 
+    it('should return 2 for [6, 6, 4, 4, 1, 3]', function() {
+        expect(calculator.electronsAroundCores([6, 6, 4, 4, 1, 3])).toEqual(2);
+    });
 
+    it('should return 12 for [3, 5, 3, 5, 4, 2]', function() {
+        expect(calculator.electronsAroundCores([3, 5, 3, 5, 4, 2])).toEqual(12);
+    });
 });
